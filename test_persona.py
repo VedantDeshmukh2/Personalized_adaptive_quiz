a56c1070c610deef89403857e8b2d71c68d8e9f1
VedantDeshmukh2 <vedant@buildfastwithai.com>
2024-09-01 15:48:10 +0530

    Update test_persona.py

--- test_persona.py
+++ test_persona.py
@@ -1,7 +1,7 @@
 import os
 from mem0 import MemoryClient
 
-client = MemoryClient(api_key="m0-Nv2d5M2z7fLAVWKyLyO6t3pImEi9Q96bMqFt0xL7")
+client = MemoryClient(api_key="KEY")
 
 user_memories = client.get_all(user_id="TestStudent")
 for i in user_memories:
